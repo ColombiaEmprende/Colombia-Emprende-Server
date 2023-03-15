86f4c90cc3833dca5ed6278af3ca5c81461336e5
ColombiaEmprende <127967227+ColombiaEmprende@users.noreply.github.com>
2023-03-15 11:36:01 -0500

    Delete De

--- routes/public.route.js
+++ routes/public.route.js
@@ -52,7 +52,6 @@ router.route("/six-stores").get((req, res) => {
 });
 router.route("/enviar-email").post((req, res, next) => {
   let mail = req.body
-  let de = mail.Nombre.toLowerCase().replace(/\s+/g, "");
   let mailOptions = {
     from: `${mail.Nombre} - Colombia Emprende`,
     to: mail.Email,
