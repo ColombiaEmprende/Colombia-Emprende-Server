2e10078142a68c97bf1d8e318fd4b9605d395843
ColombiaEmprende <127967227+ColombiaEmprende@users.noreply.github.com>
2023-03-15 11:35:28 -0500

    nameEmail

--- routes/public.route.js
+++ routes/public.route.js
@@ -54,7 +54,7 @@ router.route("/enviar-email").post((req, res, next) => {
   let mail = req.body
   let de = mail.Nombre.toLowerCase().replace(/\s+/g, "");
   let mailOptions = {
-    from: `${mail.Nombre} - Colombia Emprende <${de}@colombiaemprende>`,
+    from: `${mail.Nombre} - Colombia Emprende`,
     to: mail.Email,
     subject: mail.Subject,
     text: mail.Msj,
